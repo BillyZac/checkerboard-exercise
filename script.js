13f97f03b66be66322e6ea8b55d586d33b559d40
Zachary Smith <billyzacsmith@gmail.com>
2015-11-03 14:32:20 -0700

    Clean up and organize code. Make some helpful comments.

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+/********************************************/
+// SET UP THE BODY
+/********************************************/
 var body = document.getElementsByTagName('body')[0]
 body.style.backgroundColor = '#000000'
 body.style.margin = '50px auto'
 body.style.width = '600px'
 
-
+/********************************************/
+// BUILD THE CHECKERBOARD
+/********************************************/
 for (var i=0; i < 81; i++) {
+  // Create a square
   var square = document.createElement('div')
+
+  // Set layout properties
   square.style.width = '11.1%'
   square.style.paddingBottom = '11.1%'
-  square.style.backgroundColor = 'gray'
-  if (i%2 === 0) {
-    square.style.backgroundColor = 'tomato'
-  }
   square.style.float = 'left'
+
+  // Color the square
+  var color = 'aqua'
+  square.style.backgroundColor = color
+
+  // Put the square on the board
   body.appendChild(square)
 }
