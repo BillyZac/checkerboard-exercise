dce68ae977cd1af094cfcc561b3b34b839e9652d
Zachary Smith <billyzacsmith@gmail.com>
2015-11-03 14:45:18 -0700

    Set color using HSL. Set hue to a random value for each square, ranging between 0 and 100, i.e. from red through orange and yellow to green.

--- script.js
+++ script.js
@@ -2,9 +2,10 @@
 // SET UP THE BODY
 /********************************************/
 var body = document.getElementsByTagName('body')[0]
-body.style.backgroundColor = '#000000'
+body.style.backgroundColor = '#FFFFFF'
 body.style.margin = '50px auto'
 body.style.width = '600px'
+
 
 /********************************************/
 // BUILD THE CHECKERBOARD
@@ -19,7 +20,9 @@ for (var i=0; i < 81; i++) {
   square.style.float = 'left'
 
   // Color the square
-  var color = 'aqua'
+  var h = Math.floor(Math.random()*100) /* Set hue to a number between 0 and 100 */
+  var color = 'hsl(' + h + ', 100%, 80%)'
+  console.log(color)
   square.style.backgroundColor = color
 
   // Put the square on the board
